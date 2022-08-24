b042cef45f78551504c4f5bd73784813e9cd4c17
Daniel2231 <daniel2231.dev@gmail.com>
2022-08-24 17:09:49 +0900

    added navigation buttons

--- src/components/common/MobileDisplay.js
+++ src/components/common/MobileDisplay.js
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import React, {useCallback} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const OuterContainer = styled.div`
 	display: flex;
@@ -14,7 +16,7 @@ const InnerContainer = styled.div`
 	height: 800px;
 	width: 425px;
 	position: relative;
-	background-color: #FFFFFF;
+	background-color: #ffffff;
 	overflow-x: hidden;
 	overflow-y: scroll;
 	-webkit-overflow-scrolling: touch;
@@ -24,10 +26,70 @@ const InnerContainer = styled.div`
 	}
 `;
 
+const CustomButton = styled.button`
+	appearance: button;
+	backface-visibility: hidden;
+	background-color: rgb(95, 0, 128);
+	border-radius: 6px;
+	border-width: 0;
+	box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px inset,
+		rgba(50, 50, 93, 0.1) 0 2px 5px 0, rgba(0, 0, 0, 0.07) 0 1px 1px 0;
+	box-sizing: border-box;
+	color: #fff;
+	cursor: pointer;
+	font-size: 100%;
+	height: 44px;
+	line-height: 1.15;
+	margin: 12px 10px;
+	outline: none;
+	overflow: hidden;
+	padding: 0 25px;
+	position: relative;
+	text-align: center;
+	text-transform: none;
+	transform: translateZ(0);
+	transition: all 0.2s, box-shadow 0.08s ease-in;
+	user-select: none;
+	-webkit-user-select: none;
+	touch-action: manipulation;
+	width: 100%;
+
+	&:disabled {
+		cursor: default;
+	}
+
+	&:focus {
+		box-shadow: rgba(50, 50, 93, 0.1) 0 0 0 1px inset,
+			rgba(50, 50, 93, 0.2) 0 6px 15px 0, rgba(0, 0, 0, 0.1) 0 2px 2px 0,
+			rgba(50, 151, 211, 0.3) 0 0 0 4px;
+	}
+`;
+
+const ButtonDiv = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	width: 50%;
+	height: 50px;
+	margin: 0;
+	padding: 0;
+	margin-bottom: 20px;
+`;
+
 export default function MobileDisplay(props) {
+	const navigate = useNavigate();
+
+	const handleHome = useCallback(() => navigate('/', { replace: true }), [navigate]);
+	const handleAkinator = useCallback(() => navigate('/akinator', { replace: true }), [navigate]);
+	
 	return (
 		<OuterContainer>
 			<h1>Kurly App Emulator</h1>
+			<ButtonDiv>
+				<CustomButton onClick={handleHome}>Search</CustomButton>
+				<CustomButton onClick={handleAkinator}>Akinator</CustomButton>
+			</ButtonDiv>
 			<InnerContainer>{props.children}</InnerContainer>
 		</OuterContainer>
 	);
